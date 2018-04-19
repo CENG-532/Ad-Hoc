5c96a406a0655b900f0a1739d692bc98ba55a1ce
e2036143 <e2036143@ceng.metu.edu.tr>
2018-04-19 17:09:36 +0300

    Implementation of application layer

--- application_layer.py
+++ application_layer.py
@@ -1,7 +1,30 @@
 import zmq
+from collections import namedtuple
+import threading
 
-context =zmq.Context()
+packet = namedtuple("packet", ["type", "source", "destination", "next_hop", "position", "message"])
 
-socket = context.socket(zmq.REP)
 
-socket.bind("tcp://*:5555")
+def get_message_to_send(context):
+    client_socket = context.socket(zmq.REQ)
+    # get a message from user and send it
+    while True:
+        input_string = input("message dest: ")
+        message, destination = input_string.split(" ")
+        packet_to_send = packet("DATA", None, destination, None, None, message)
+        client_socket.send(packet_to_send)
+
+
+if __name__ == "__main__":
+    context = zmq.Context()
+    server_socket = context.socket(zmq.REP)
+    server_socket.bind("tcp://*:5555")
+    send_thread = threading.Thread(target=get_message_to_send, args=[context])
+
+    while True:
+        received_message = server_socket.recv()
+        # process received message here
+        print(received_message)
+        # break on some condition
+
+    send_thread.join()
