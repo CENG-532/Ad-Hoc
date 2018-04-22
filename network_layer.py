c11d8f1cd781b629a4a2ab57e1389d9a5cc16e94
goksgie <goksgie@gmail.com>
2018-04-22 23:16:04 +0300

    Implement periodic update and packet process

--- network_layer.py
+++ network_layer.py
@@ -10,7 +10,7 @@ import time
 from collections import namedtuple
 
 packet = namedtuple("packet",
-                    ["type", "source", "name", "sequence", "link state", "destination", "next_hop",
+                    ["type", "source", "name", "sequence", "link_state", "destination", "next_hop",
                      "position",
                      "message"])
 
@@ -19,16 +19,20 @@ routing_table_mutex = threading.Lock()
 
 neighbor_list = []  # nodes that are adjacent to the current node. (name, pos)
 
-topology_table = {"link state": {}, "sequence": {}}  # will consist of link state information of node j and the sequence
+topology_table = {}
+# topology_table = {"name": {"address":, "sequence":, "lastHeardTime":, "neighbor_list":, "needToSend:" bool}}
 
+routing_table = {}
 
 sequence = 0
 
-clock_interval_by_hops = []
+scope_interval = []
+
+scope_clocks = []
 
 fish_eye_ranges = []
 
-number_of_scope = 2
+number_of_scopes = 2
 
 known_nodes = []
 
@@ -52,6 +56,10 @@ position_self = None
 
 link_layer_port_number = None
 
+topology_table_changed = False
+
+max_last_heard_time = None
+
 
 # here define rooting algorithm
 
@@ -59,7 +67,8 @@ link_layer_port_number = None
 
 def node_init():
     # the initialization message that all nodes are going to send to each other.
-    message = packet("Pbroad", ip_address_self, name_self, sequence, "", ("255.255.255.255", link_layer_port_number),
+    message = packet("Pbroad", ip_address_self, name_self, sequence, topology_table,
+                     ("255.255.255.255", link_layer_port_number),
                      "", position_self, "")
     link_layer_message_queue.put(message)
 
@@ -71,6 +80,7 @@ def calculate_distance(pos1, pos2):
 def find_shortest_path():
     global position_self
     global routing_table
+    global topology_table_changed
 
     # dijkstra shortest-path algorithm
     routing_table[name_self] = {"dest_addr": ip_address_self, "next_hop": ip_address_self, "distance": 0}
@@ -84,7 +94,6 @@ def find_shortest_path():
             else:
                 routing_table[x]["distance"] = math.inf
                 routing_table[x]["next_hop"] = -1
-
 
     is_changed = False
     while list(set(known_nodes) - set(p)):
@@ -107,21 +116,40 @@ def find_shortest_path():
             routing_table[min_k]["distance"] = min_distance
             routing_table[min_k]["next_hop"] = routing_table[min_l]["next_hop"]
 
+    topology_table_changed = False
+
 
 def process_packet(message):
+    global topology_table, topology_table_changed
+    global sequence
+
+    topology_table_changed = False
+
     source = message.source
     name = message.name
+    position = message.position
     packet_sequence = int(message.sequence)
     packet_link_state = message.link_state
-    try:
-        topology_table["link state"][name] = topology_table["link state"][name] + source
-    except KeyError:
-        topology_table["link state"][name] = [source]  # currently under development
 
-    for node_name in known_nodes:
-        if node_name != name_self and packet_sequence > topology_table["sequence"][node_name]:
-            topology_table["sequence"][node_name] = packet_sequence
-            topology_table["link state"][node_name] = packet_link_state
+    if name in topology_table[name_self]["neighbor_list"]:
+        topology_table[name]["position"] = position
+        topology_table[name]["sequence"] += 1
+    else:
+        topology_table[name_self]["neighbor_list"].append(name)
+
+    for dest_in_packet in packet_link_state:
+        if dest_in_packet not in topology_table:
+            topology_table[dest_in_packet] = packet_link_state[dest_in_packet]
+            topology_table[dest_in_packet]["sequence_number"] += 1
+            topology_table[dest_in_packet]["last_heard_time"] = time.time()
+            topology_table_changed = True
+        else:
+            if topology_table[dest_in_packet]["sequence_number"] < packet_link_state[dest_in_packet]["sequence_number"]:
+                topology_table[dest_in_packet] = packet_link_state[dest_in_packet]
+                topology_table[dest_in_packet]["last_heard_time"] = time.time()
+                topology_table_changed = True
+            else:
+                topology_table[name]["needToSend"] = True
 
 
 def check_neighbors():
@@ -137,34 +165,55 @@ def check_neighbors():
         topology_table[name_self]["neighbor_list"].remove(neighbor)
 
 
+def clear_neighbors(to_be_deleted_neighbors):
+    for neighbor in to_be_deleted_neighbors:
+        topology_table[name_self]["neighbor_list"].remove(neighbor)
+
+
+def check_elapsed_time(current_time, scope):
+    is_changed = (current_time - scope_clocks[scope]) * 1000 > scope_interval[scope]
+    if is_changed:
+        scope_clocks[scope] = time.time()
+    return is_changed
+
+
 def periodic_routing_update():
-    global sequence
+    global sequence, max_last_heard_time, number_of_scopes
     sequence += 1
 
     # I have added necessary parts roughly. We can check both the packet type and structural design tomorrow.
 
-    topology_table["link state"][name_self] = []
-    topology_table["sequence"][name_self] = sequence
+    to_be_deleted_neighbors = []
 
-    message = packet("Pupdate", ip_address_self, name_self, sequence, "link state here", "255.255.255.255", "",
+    current_time = time.time()
+
+    for neighbor in topology_table[name_self]["neighbor_list"]:
+        if topology_table[neighbor]["last_heard_time"] + max_last_heard_time < current_time:
+            to_be_deleted_neighbors.append(neighbor)
+
+    clear_neighbors(to_be_deleted_neighbors)
+
+    message = packet("broadcast", ip_address_self, name_self, sequence, {}, "255.255.255.255", "",
                      position_self, "")
 
-    for node in neighbor_list:
-        topology_table["link state"][name_self].append(node)
+    inserted_nodes = []
 
-    for node, _ in known_nodes:
-        for scope in range(number_of_scope):
-            clock = clock_interval_by_hops[scope]
-            fish_eye_range = fish_eye_ranges[scope]
-            if distance_table[node] < fish_eye_range and clock % 1000:
-                message._replace(topology_table=message.topology_table + topology_table["link state"][node])
+    for scope in range(number_of_scopes):
+        fish_eye_range = fish_eye_ranges[scope]
+        is_time_elapsed = check_elapsed_time(current_time, scope)
+        for node in known_nodes:
+            if node not in inserted_nodes and routing_table[node]["distance"] < fish_eye_range and is_time_elapsed:
+                message.link_state[node] = topology_table[node]
+                inserted_nodes.append(node)
+
+    link_layer_message_queue.put(message)
 
 
 def find_routing(destination):
     # check routing table to find the next hop.
     routing_table_mutex.acquire()
     try:
-        next_hop = topology_table[destination]["next_hop"]
+        next_hop = routing_table[destination]["next_hop"]
     except KeyError:
         next_hop = ""
     routing_table_mutex.release()
@@ -184,13 +233,13 @@ def update_routing_table(message):
     process_packet(message)
     # here reset the topology table I guess,
     # we need to check the structure here as well.
-    topology_table["link state"][name_self] = neighbor_list
+    topology_table["link_state"][name_self] = neighbor_list
     find_shortest_path()
     periodic_routing_update()
 
 
 def _is_control_message(message_type):
-    return message_type == "broad" or message_type == "update"
+    return message_type == "broadcast" or message_type == "update"
 
 
 def _is_destination_self(destination):
@@ -236,18 +285,17 @@ def link_layer_client():
     while True:
         message = link_layer_message_queue.get()
 
-        next_hop = find_routing(message.destination)
-
-        message._replace(next_hop=find_routing(message.destination))
+        if not _is_control_message(message.type):
+            message.next_hop = find_routing(message.destination)
 
         client_socket.send(pickle.dumps(message))
 
-    pass
-
 
 def read_config_file(filename, name):
-    global ip_address_self, name_self, position_self, clock_interval_by_hops
-    global number_of_scope, port_number_self, link_layer_port_number
+    global ip_address_self, name_self, position_self, scope_interval
+    global number_of_scopes, port_number_self, link_layer_port_number
+    global max_last_heard_time
+    global scope_clocks
 
     name_self = name
     config = configparser.ConfigParser()
@@ -260,15 +308,21 @@ def read_config_file(filename, name):
     ip_address_self = (port_read[0], port_number_self)
     link_layer_port_number = default_settings["link_layer_port_number"]
 
-    clock_interval_by_hops.append(int(default_settings["clock_interval_hop_1"]))
-    clock_interval_by_hops.append(int(default_settings["clock_interval_hop_2"]))
+    scope_interval.append(int(default_settings["scope_interval_1"]))
+    scope_interval.append(int(default_settings["scope_interval_2"]))
 
     fish_eye_ranges.append(int(default_settings["fish_eye_range_1"]))
     fish_eye_ranges.append(int(default_settings["fish_eye_range_2"]))
 
-    number_of_scope = len(fish_eye_ranges)
+    max_last_heard_time = int(default_settings["max_last_heard_time"])
+
+    number_of_scopes = len(fish_eye_ranges)
 
     position_self = (float(node_settings["positionX"]), float(node_settings["positionX"]))
+
+    current_time = time.time()
+
+    scope_clocks = [current_time for _ in scope_interval]
 
 
 if __name__ == "__main__":
